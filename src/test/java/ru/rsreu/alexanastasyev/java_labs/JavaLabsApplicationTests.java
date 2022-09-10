d4c725a58afc24f2c806c6508176152003e07f47
alexanastasyev <alex.anastasyev@mail.ru>
2022-09-10 12:54:32 +0300

    Initial commit: lab1

---
+++ src/test/java/ru/rsreu/alexanastasyev/java_labs/JavaLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsreu.alexanastasyev.java_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
